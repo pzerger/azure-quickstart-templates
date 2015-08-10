847b0bb9078a7b3557c022a01c115a32b15b75e8
Ryan Irujo <ryan.irujo@gmail.com>
2015-08-10 20:37:48 +0200

    Added DNS Management Tools to Install-AD-Mgmt-Tools.ps1 Script.

--- ad-deploy-new-domain/Install-AD-Mgmt-Tools.ps1
+++ ad-deploy-new-domain/Install-AD-Mgmt-Tools.ps1
@@ -1,5 +1,5 @@
 # Installing Active Directory Management Tools
-Install-WindowsFeature -Name RSAT-AD-Tools,RSAT-AD-PowerShell,RSAT-ADDS
+Install-WindowsFeature -Name RSAT-AD-Tools,RSAT-AD-PowerShell,RSAT-ADDS,RSAT-DNS-Server
 
 # Creating Text File stating Installation was Completed.
 $ADToolsInstallComplete = [System.IO.File]::Create("C:\Windows\Temp\AD_Tools_Install_Complete.txt").Close()
