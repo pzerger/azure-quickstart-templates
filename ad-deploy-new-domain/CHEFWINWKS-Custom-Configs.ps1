38a0859037c486a463165c84df86f4cf9ae6c79d
Ryan Irujo <ryan.irujo@gmail.com>
2015-08-19 20:18:43 +0200

    Added Chef Server SSL Certificate Fetch Script

--- ad-deploy-new-domain/CHEFWINWKS-Custom-Configs.ps1
+++ ad-deploy-new-domain/CHEFWINWKS-Custom-Configs.ps1
@@ -8,8 +8,6 @@
 - Downloads and Installs ChefDK.
 - Download the 'knife.rb' for this Environment from GitHub.
 - Downloads and Installs Notepad++.
-- Retrieves the Chef Server SSL Certificate using knife.
-- Copies the Chef Server Certificate to the the Chef Client 'trusted_certs' directory.
 - File(s) are created in 'C:\Windows\Temp' stating whether the actions listed above were successful or not.
 #>
 
@@ -156,29 +154,3 @@ If (!$?)
 	{
 		[System.IO.File]::Create("C:\Windows\Temp\_NotepadPlusPlus_Install_Failed.txt").Close()
 	}
-
-# Retrieving the Chef Server SSL Certificate using knife.
-cd "C:\chef\.chef"
-knife ssl fetch
-
-If ($?)
-	{
-		[System.IO.File]::Create("C:\Windows\Temp\_Chef_Server_SSL_Cert_Retrieved_Successfully.txt").Close()
-	}	
-If (!$?)
-	{
-		[System.IO.File]::Create("C:\Windows\Temp\_Chef_Server_SSL_Cert_Retrieval_Failed.txt").Close()
-	}
-
-# Copying the Chef Server Certificate to the the Chef Client 'trusted_certs' directory
-cp "C:\chef\.chef\trusted_certs\*" "C:\chef\trusted_certs\"
-
-If ($?)
-	{
-		[System.IO.File]::Create("C:\Windows\Temp\_Chef_Server_SSL_Cert_Copy_To_Chef_Client_Dir_Successfully.txt").Close()
-	}	
-If (!$?)
-	{
-		[System.IO.File]::Create("C:\Windows\Temp\_Chef_Server_SSL_Cert_Copy_To_Chef_Client_Dir_Failed.txt").Close()
-	}
-
