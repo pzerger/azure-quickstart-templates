41d2f8396e8296dda3889a32562e085c38e2cb5b
Ryan Irujo <ryan.irujo@gmail.com>
2015-08-21 14:35:33 +0200

    Added Cookbook Chef_Client_Win_Service_10_MIC

---
+++ ad-deploy-new-domain/Cookbooks/Install_Chef_Client_Win_Service_10_MIC/README.md
@@ -0,0 +1,4 @@
+# Install_Chef_Client_Win_Service_10_MIC
+
+Installs and Configures the Chef Client as a Windows Service and to run every 10 minutes.
+
---
+++ ad-deploy-new-domain/Cookbooks/Install_Chef_Client_Win_Service_10_MIC/test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'Install_Chef_Client_Win_Service_10_MIC::default' do
+  # Serverspec examples can be found at
+  # http://serverspec.org/resource_types.html
+  it 'does something' do
+    skip 'Replace this with meaningful tests'
+  end
+end
