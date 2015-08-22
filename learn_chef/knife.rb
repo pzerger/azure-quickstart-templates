9f2b2fe088654ccb3ddfff62b4f308e63ca0ecd8
Ryan Irujo <ryan.irujo@gmail.com>
2015-08-22 16:55:12 +0200

    Changed cookbook_path to use forward slashes for path
    Changed this from using back slashes to forward slashes as this was
    causing knife upload cookbook function to return a Ruby  ArgumentError
    instead of uploading the targeted cookbook.

--- learn_chef/knife.rb
+++ learn_chef/knife.rb
@@ -6,5 +6,5 @@ validation_client_name   'learn_chef_12_env-validator'
 validation_key           'C:/Chef/.chef/learn_chef_12_env-validator.pem' 
 chef_server_url          'https://chefsrv.contoso.corp/organizations/learn_chef_12_env' 
 syntax_check_cache_path  'C:/Chef/.chef/syntax_check_cache' 
-cookbook_path [ 'C:\Chef\chef-repo\cookbooks' ] 
+cookbook_path [ 'C:/Chef/chef-repo/cookbooks' ] 
 knife[:editor] = '"C:\Program Files (x86)\Notepad++\notepad++.exe" -nosession -multiInst'
