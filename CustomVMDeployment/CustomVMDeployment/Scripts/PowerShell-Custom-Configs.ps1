de4e9b0802fe9d422177ec744b6b466ada700cc9
Ryan Irujo <ryan.irujo@gmail.com>
2015-09-10 13:28:12 +0200

    Updated Scripts and Files

--- CustomVMDeployment/CustomVMDeployment/Scripts/PowerShell-Custom-Configs.ps1
+++ CustomVMDeployment/CustomVMDeployment/Scripts/PowerShell-Custom-Configs.ps1
@@ -7,89 +7,128 @@
 - File(s) are created in 'C:\Windows\Temp' stating whether the actions listed above were successful or not.
 #>
 
-# Disabling IE ESC for Administrators on Target Host. UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-$Disable_IE_ESC_Admins = New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name IsInstalled -Value 0 -Force
+$TempDir = "C:\Windows\Temp"
 
-if ($Disable_IE_ESC_Admins.IsInstalled -eq 0)
+If(([System.IO.File]::Exists("$($TempDir)\_IE_ESC_For_Admins_Disabled_Failed.txt")) -or (![System.IO.File]::Exists("$($TempDir)\_IE_ESC_For_Admins_Disabled_Successfully.txt")))
 	{
-		[System.IO.File]::Create("C:\Windows\Temp\_IE_ESC_For_Admins_Disabled_Sucessfully.txt").Close()
+		# Removing previous run 'Failed' Text File if it exists.
+		[System.IO.File]::Delete("$($TempDir)\_IE_ESC_For_Admins_Disabled_Failed.txt")
+		
+		# Disabling IE ESC for Administrators on Target Host. UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+		$Disable_IE_ESC_Admins = New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name IsInstalled -Value 0 -Force
+		if ($Disable_IE_ESC_Admins.IsInstalled -eq 0)
+			{
+				[System.IO.File]::Create("$($TempDir)\_IE_ESC_For_Admins_Disabled_Successfully.txt").Close()
+			}
+			
+		if ($Disable_IE_ESC_Admins.IsInstalled -ne 0)
+			{
+				[System.IO.File]::Create("$($TempDir)\_IE_ESC_For_Admins_Disabled_Failed.txt").Close()
+			}
 	}
 	
-if ($Disable_IE_ESC_Admins.IsInstalled -ne 0)
+If(([System.IO.File]::Exists("$($TempDir)\_WinRM_Allow_Unencrypted_Enabled_Failed.txt")) -or (![System.IO.File]::Exists("$($TempDir)\_WinRM_Allow_Unencrypted_Enabled_Successfully.txt")))
 	{
-		[System.IO.File]::Create("C:\Windows\Temp\_IE_ESC_For_Admins_Disabled_Failed.txt").Close()
-	}
-
-# Setting WinRM to allow Unencrypted traffic and setting Authentication to Basic
-$AllowUnencrypted = winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-
-If ($?)
-	{
-		[System.IO.File]::Create("C:\Windows\Temp\_WinRM_Allow_Unencrypted_Enabled_Sucessfully.txt").Close()
-	}
-If (!$?)
-	{
-		[System.IO.File]::Create("C:\Windows\Temp\_WinRM_Allow_Unencrypted_Enabled_Failed.txt").Close()
-	}
-
-$AllowBasicAuth = winrm set winrm/config/service/auth '@{Basic="true"}'
-
-If ($?)
-	{
-		[System.IO.File]::Create("C:\Windows\Temp\_WinRM_Allow_Basic_Auth_Enabled_Sucessfully.txt").Close()
-	}
-If (!$?)
-	{
-		[System.IO.File]::Create("C:\Windows\Temp\_WinRM_Allow_Basic_Auth_Enabled_Failed.txt").Close()
+		# Removing previous run 'Failed' Text File if it exists.
+		[System.IO.File]::Delete("$($TempDir)\_WinRM_Allow_Unencrypted_Enabled_Failed.txt")
+		
+		# Setting WinRM to allow Unencrypted traffic.
+		$AllowUnencrypted = winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+		If ($?)
+			{
+				[System.IO.File]::Create("$($TempDir)\_WinRM_Allow_Unencrypted_Enabled_Successfully.txt").Close()
+			}
+		If (!$?)
+			{
+				[System.IO.File]::Create("$($TempDir)\_WinRM_Allow_Unencrypted_Enabled_Failed.txt").Close()
+			}
 	}
 	
-#Enabling .NET Framework 3.5 for SQL Install.
-DISM /Online /Enable-Feature /FeatureName:NetFx3 /All
-
-If ($?)
+If(([System.IO.File]::Exists("$($TempDir)\_WinRM_Allow_Basic_Auth_Enabled_Failed.txt")) -or (![System.IO.File]::Exists("$($TempDir)\_WinRM_Allow_Basic_Auth_Enabled_Successfully.txt")))	
 	{
-		[System.IO.File]::Create("C:\Windows\Temp\_dotNET_Framework_35_Enabled_Sucessfully.txt").Close()
+		# Removing previous run 'Failed' Text File if it exists.
+		[System.IO.File]::Delete("$($TempDir)\_WinRM_Allow_Basic_Auth_Enabled_Failed.txt")
+			
+		# Setting WinRM Authentication to Basic.
+		$AllowBasicAuth = winrm set winrm/config/service/auth '@{Basic="true"}'
+		If ($?)
+			{
+				[System.IO.File]::Create("$($TempDir)\_WinRM_Allow_Basic_Auth_Enabled_Successfully.txt").Close()
+			}
+		If (!$?)
+			{
+				[System.IO.File]::Create("$($TempDir)\_WinRM_Allow_Basic_Auth_Enabled_Failed.txt").Close()
+			}
 	}
-If (!$?)
+
+If(([System.IO.File]::Exists("$($TempDir)\_dotNET_Framework_35_Enabled_Failed.txt")) -or (![System.IO.File]::Exists("$($TempDir)\_dotNET_Framework_35_Enabled_Successfully.txt")))	
 	{
-		[System.IO.File]::Create("C:\Windows\Temp\_dotNET_Framework_35_Enabled_Failed.txt").Close()
+		# Removing previous run 'Failed' Text File if it exists.
+		[System.IO.File]::Delete("$($TempDir)\_dotNET_Framework_35_Enabled_Failed.txt")
+			
+		#Enabling .NET Framework 3.5 for SQL Install.
+		DISM /Online /Enable-Feature /FeatureName:NetFx3 /All
+		If ($?)
+			{
+				[System.IO.File]::Create("$($TempDir)\_dotNET_Framework_35_Enabled_Successfully.txt").Close()
+			}
+		If (!$?)
+			{
+				[System.IO.File]::Create("$($TempDir)\_dotNET_Framework_35_Enabled_Failed.txt").Close()
+			}
+	}
+
+If(([System.IO.File]::Exists("$($TempDir)\_Create_Chef_Client_Directories_Failed.txt")) -or (![System.IO.File]::Exists("$($TempDir)\_Create_Chef_Client_Directories_Successfully.txt")))
+	{
+		# Removing previous run 'Failed' Text File if it exists.
+		[System.IO.File]::Delete("$($TempDir)\_Create_Chef_Client_Directories_Failed.txt")
+			
+		# Creating 'C:\Chef\trusted_certs' directory for the Chef Client.
+		[System.IO.Directory]::CreateDirectory("C:\chef\trusted_certs")
+		If ($?)
+			{
+				[System.IO.File]::Create("$($TempDir)\_Create_Chef_Client_Directories_Successfully.txt").Close()
+			}
+		If (!$?)
+			{
+				[System.IO.File]::Create("$($TempDir)\_Create_Chef_Client_Directories_Failed.txt").Close()
+			}
 	}
 	
-# Creating 'C:\Chef\trusted_certs' directory for the Chef Client.
-[System.IO.Directory]::CreateDirectory("C:\chef\trusted_certs")
-
-If ($?)
+If(([System.IO.File]::Exists("$($TempDir)\_NotepadPlusPlus_Download_Failed.txt")) -or (![System.IO.File]::Exists("$($TempDir)\_NotepadPlusPlus_Downloaded_Successfully.txt")))
 	{
-		[System.IO.File]::Create("C:\Windows\Temp\_Create_Chef_Client_Directories_Sucess.txt").Close()
+		# Removing previous run 'Failed' Text File if it exists.
+		[System.IO.File]::Delete("$($TempDir)\_NotepadPlusPlus_Download_Failed.txt")
+			
+		# Download Notepad++
+		$Notepad_WebClient = New-Object System.Net.WebClient
+		$Notepad_URI       = "https://notepad-plus-plus.org/repository/6.x/6.8.1/npp.6.8.1.Installer.exe"
+		$Notepad_File      = "$($TempDir)\npp.6.8.1.Installer.exe"
+		$Notepad_WebClient.DownloadFile($Notepad_URI,$Notepad_File)
+		If ($?)
+			{
+				[System.IO.File]::Create("$($TempDir)\_NotepadPlusPlus_Downloaded_Successfully.txt").Close()
+			}
+		If (!$?)
+			{
+				[System.IO.File]::Create("$($TempDir)\_NotepadPlusPlus_Download_Failed.txt").Close()
+			}
 	}
-If (!$?)
+	
+If(([System.IO.File]::Exists("$($TempDir)\_NotepadPlusPlus_Install_Failed.txt")) -or (![System.IO.File]::Exists("$($TempDir)\_NotepadPlusPlus_Installed_Successfully.txt")))	
 	{
-		[System.IO.File]::Create("C:\Windows\Temp\_Create_Chef_Client_Directories_Failed.txt").Close()
-	}
+		# Removing previous run 'Failed' Text File if it exists.
+		[System.IO.File]::Delete("$($TempDir)\_NotepadPlusPlus_Install_Failed.txt")
+			
+		# Install Notepad++
+		& "$($TempDir)\npp.6.8.1.Installer.exe" /S
 
-# Download Notepad++
-$Notepad_WebClient = New-Object System.Net.WebClient
-$Notepad_URI       = "https://notepad-plus-plus.org/repository/6.x/6.8.1/npp.6.8.1.Installer.exe"
-$Notepad_File      = "C:\Windows\Temp\npp.6.8.1.Installer.exe"
-$Notepad_WebClient.DownloadFile($Notepad_URI,$Notepad_File)
-
-If ($?)
-	{
-		[System.IO.File]::Create("C:\Windows\Temp\_NotepadPlusPlus_Downloaded_Successfully.txt").Close()
-	}
-If (!$?)
-	{
-		[System.IO.File]::Create("C:\Windows\Temp\_NotepadPlusPlus_Download_Failed.txt").Close()
-	}
-
-# Install Notepad++
-C:\Windows\Temp\npp.6.8.1.Installer.exe /S
-
-If ($?)
-	{
-		[System.IO.File]::Create("C:\Windows\Temp\_NotepadPlusPlus_Installed_Successfully.txt").Close()
-	}
-If (!$?)
-	{
-		[System.IO.File]::Create("C:\Windows\Temp\_NotepadPlusPlus_Install_Failed.txt").Close()
+		If ($?)
+			{
+				[System.IO.File]::Create("$($TempDir)\_NotepadPlusPlus_Installed_Successfully.txt").Close()
+			}
+		If (!$?)
+			{
+				[System.IO.File]::Create("$($TempDir)\_NotepadPlusPlus_Install_Failed.txt").Close()
+			}
 	}
